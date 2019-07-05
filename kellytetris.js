0567b5b1503a4516c034c8a20dee00453746a402
gamercool <support@minepex.ru>
2019-07-05 16:45:09 +0300

    Update kellytetris.js

--- kellytetris.js
+++ kellytetris.js
@@ -371,7 +371,7 @@ KellyTetris.playMe = function() {
 		
 		KellyTetris.consoleType({
 			starttext : 'C:\\',
-			text : 'Users\\User\\>',
+			text : 'Users\\User\\> \n C:\\Users\\User\\>cd Tetris \n C:\\User\\User\\Tetris\\>start tetris \n Запуск игры Tetris...',
 			delayBefore : 5,
 			delayAfter : 5,
 			screen : game,
